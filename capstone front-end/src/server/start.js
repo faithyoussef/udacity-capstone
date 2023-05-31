5cd9b09e131bce4b71ea810b0040beaa27a83ec8
faithyoussef <38753427+faithyoussef@users.noreply.github.com>
2023-05-31 17:15:37 +0300

    Delete capstone front-end directory

--- capstone front-end/src/server/start.js
+++
@@ -1,7 +0,0 @@
-const app = require('./server')
-
-// Designates what port the app will listen to for incoming requests
-const port = 8081
-app.listen(port,
-    () => console.log(`Travel weather app listening on port ${port}!`)
-)
--- capstone front-end/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    entry:'src/client/index.js',
-    output:{
-    resolve:{
-       extensions:['.js','.jsx']
-    }
-    },
- }
