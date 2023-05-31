0aa4ac9d07303af343aeff337b5297fec9ce594f
faithyoussef <38753427+faithyoussef@users.noreply.github.com>
2023-05-31 13:21:25 +0300

    Add files via upload

--- capstone front-end/__test__/server.test.js
+++ capstone front-end/__test__/server.test.js
@@ -1,4 +1,4 @@
-import app from '../src/server/server.js'
+import app from 'src/server/server.js'
 import supertest from 'supertest'
 const request = supertest(app)
 
---
+++ capstone front-end/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports={
+    entry:'src/client/index.js',
+    output:{
+    resolve:{
+       extensions:['.js','.jsx']
+    }
+    },
+ }
